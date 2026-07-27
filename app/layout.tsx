59bbe074180bc736067351cb90dbcfbd0bbb9b99
Ayesha Azeem <ayeshaaazeem690@gmail.com>
2026-07-27 16:36:54 +0500

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -134,17 +134,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         />
         {/* End Google Tag Manager */}
         <meta name="google-site-verification" content="AidnHmuErYZJwn9izLxLIYD-flhpMD63ls7Qyt_KrpY" />
-        {/* Non-legit GTM container commented out
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5Q2BMHLR');`,
-          }}
-        />
-        */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11186441600" />
         <script
           dangerouslySetInnerHTML={{
@@ -234,16 +223,6 @@ gtag('config', 'AW-11186441600');`,
           />
         </noscript>
         {/* End Google Tag Manager (noscript) */}
-        {/* Non-legit GTM container commented out
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5Q2BMHLR"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-        */}
         {children}
         <GlobalChatbot />
         <Analytics />
